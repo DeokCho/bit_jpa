6adc4bf795c5d1da13b8cbe779df6022597e2285
조덕행 <62981065+DeokCho@users.noreply.github.com>
2020-05-04 15:11:32 +0900

    2020.05.04

--- occamsrazor-2/src/main/resources/static/resources/js/admin.js
+++ occamsrazor-2/src/main/resources/static/resources/js/admin.js
@@ -1,13 +1,45 @@
 "use strict"
+
 var admin = admin || {}
-admin = (()=>{
-		let init = () =>{
-			onCreate()
-		}
-		let onCreate = () =>{
-			setContentView()
-		}
-		let setContentView = () =>{
-		}
-		return {init}
-	})()
+
+admin = (() => {
+	let init = () => {
+		onCreate()
+	}
+	let onCreate = () => {
+		setContentView()
+		$.getJSON('/users', d=>{
+			$.each(d, (i,j)=>{
+				$(`<tr>
+				<tr>
+                <td>
+                    <span>1.</a>
+                </td>
+                <td>
+                    <span>1</a>
+                </td>
+                 <td>
+                    <span>1</a>
+                </td>
+               <td>
+                    <span>1</a>
+                </td>
+                <td>
+                    <span>1</a>
+                </td>
+                <td>
+                    <span>1</a>
+                </td>
+                <td>
+                    <span>1</a>
+                </td>
+                </tr>`).appendTo('#userData')
+			})
+		})
+	}
+	let setContentView = () => {
+		 $('#userData tr').first().css({'background-color' : 'yellow'})
+	}
+	return {init}
+	
+})()
